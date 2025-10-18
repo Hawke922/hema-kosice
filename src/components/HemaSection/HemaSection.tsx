0bf429d234aa4b0475c673439d4346054d587989
Tomas Klucar <tom.klucar@gmail.com>
2025-10-18 21:03:23 +0200

    scrolling in hemasection is vertical

--- src/components/HemaSection/HemaSection.tsx
+++ src/components/HemaSection/HemaSection.tsx
@@ -15,32 +15,30 @@ const HemaSection = ({ imagePaths }: HemaSectionProps) => {
 
   const scrollAccumulatorRef = useRef(0);
 
-  const {
-    targetRef: sectionRef,
-    isIntersecting,
-    entryDirection,
-  } = useDirectionalIntersection();
+  const { targetRef, isIntersecting, entryDirection } =
+    useDirectionalIntersection<HTMLDivElement>();
 
   useEffect(() => {
     if (!isIntersecting || !entryDirection) return;
 
     if (entryDirection === "top") {
       setCurrentImageIndex(0);
-    } else if (entryDirection === "bottom") {
+    } else {
       setCurrentImageIndex(imagePaths.length - 1);
     }
   }, [isIntersecting, entryDirection, imagePaths.length]);
 
   useEffect(() => {
     if (!isIntersecting) return;
+
     const handleWheel = (e: WheelEvent) => {
-      const atStart = currentImageIndex === 0;
-      const atEnd = currentImageIndex === imagePaths.length - 1;
+      const isAtStart = currentImageIndex === 0;
+      const isAtEnd = currentImageIndex === imagePaths.length - 1;
 
       const isScrollingUp = e.deltaY < 0;
       const isScrollingDown = e.deltaY > 0;
 
-      if ((atStart && isScrollingUp) || (atEnd && isScrollingDown)) return;
+      if ((isAtStart && isScrollingUp) || (isAtEnd && isScrollingDown)) return;
 
       e.preventDefault();
 
@@ -63,35 +61,35 @@ const HemaSection = ({ imagePaths }: HemaSectionProps) => {
   }, [isIntersecting, currentImageIndex, imagePaths.length]);
 
   return (
-    <section ref={sectionRef} className={classes.section} id="hema">
-      <div className={classes.imageContainer}>
-        <img
-          className={classes.image}
-          src={imagePaths[currentImageIndex]}
-          alt={`HEMA action ${currentImageIndex + 1}`}
-        />
-        <div className={classes.imageIndexContainer}>
+    <section className={classes.section} id="hema">
+      <img
+        className={classes.image}
+        src={imagePaths[currentImageIndex]}
+        alt={`HEMA action ${currentImageIndex + 1}`}
+      />
+      <div ref={targetRef} className={classes.textContainer}>
+        <p
+          className={`${classes.text} ${
+            isIntersecting ? classes["text--visible"] : ""
+          }`}
+        >
+          Historical European Martial Arts (HEMA) is the study and practice of
+          martial techniques from Europe, primarily from the Middle Ages to the
+          early modern period. It encompasses a wide range of fighting styles,
+          including swordsmanship, grappling, and weapon-based combat, often
+          reconstructed from historical manuals and treatises.
+        </p>
+        <div className={classes.indexContainer}>
           {imagePaths.map((path, index) => (
             <div
               key={path}
-              className={`${classes.imageIndex} ${
-                classes[currentImageIndex === index ? "imageIndex--active" : ""]
+              className={`${classes.index} ${
+                classes[currentImageIndex === index ? "index--active" : ""]
               }`}
             />
           ))}
         </div>
       </div>
-      <p
-        className={`${classes.text} ${
-          isIntersecting ? classes["text--visible"] : ""
-        }`}
-      >
-        Historical European Martial Arts (HEMA) is the study and practice of
-        martial techniques from Europe, primarily from the Middle Ages to the
-        early modern period. It encompasses a wide range of fighting styles,
-        including swordsmanship, grappling, and weapon-based combat, often
-        reconstructed from historical manuals and treatises.
-      </p>
     </section>
   );
 };
