a4f3d0da18f8e59a6d3f817e809ffd25d35cd44d
Tomas Klucar <tom.klucar@gmail.com>
2025-11-27 19:09:19 +0100

    moved join section higher

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,8 @@ const App = () => (
       <LandingSection />
       <HemaSection />
       <WeaponsSection />
-      <TeamSection />
       <JoinSection />
+      <TeamSection />
       <FaqSection />
     </main>
     <Footer />
